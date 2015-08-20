d0814b5e21679a4f92ebb0589151511b54f80bc0
jan.stria <stria.jan@gmail.com>
2015-08-20 15:47:09 +0200

    Updated mex make file to use OpenMP.

--- make.m
+++ make.m
@@ -2,7 +2,7 @@ fprintf('Removing old binaries...\n');
 delete *.mexw64
 
 fprintf('Compiling DP algorithm for open curves...\n');
-mex dp_open_mex.cpp
+mex dp_open_mex.cpp COMPFLAGS="/openmp $COMPFLAGS"
 
 fprintf('Compiling DP algorithm for close curves...\n');
-mex dp_close_mex.cpp
+mex dp_close_mex.cpp COMPFLAGS="/openmp $COMPFLAGS"
