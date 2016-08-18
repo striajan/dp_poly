4a0fb6fb0bbefd2e5cbf039b2c08359754344df4
jan.stria <stria.jan@gmail.com>
2016-08-18 10:17:11 +0200

    Removed double quotation marks.

--- dp_poly_setup.py
+++ dp_poly_setup.py
@@ -2,9 +2,9 @@ from distutils.core import setup, Extension
 from Cython.Build import cythonize
 
 setup(ext_modules = cythonize(Extension(
-    "dp_poly",
-    sources=["dp_poly.pyx"],
-    language="c++",
+    'dp_poly',
+    sources=['dp_poly.pyx'],
+    language='c++',
     extra_compile_args=['-std=c++11', '-fopenmp'],
     extra_link_args=['-lgomp'],
 )))
