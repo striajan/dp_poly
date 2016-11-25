0f8e7cbaba1e9e3a7249a0c448cade0991554e54
janstria <stria.jan@gmail.com>
2016-11-25 15:06:58 +0100

    Documentation of polygonal approximation wrapper in Python.

--- dp_poly.pyx
+++ dp_poly.pyx
@@ -6,7 +6,21 @@ cdef extern from "dp_poly_python.hpp":
     vector[size_t] dp_poly_close[T](vector[pair[T,T]] pts, size_t nVert)
 
 def open(pts, num_vert):
+    """Approximate the given open curve by a polyline
+    having the specified number of vertices.
+
+    :type pts: list[(float, float)] | numpy.ndarray
+    :type num_vert: int
+    :rtype: list[int]
+    """
     return dp_poly_open[double](pts, num_vert)
 
 def close(pts, num_vert):
+    """Approximate the given closed curve by a polygon
+    having the specified number of vertices.
+
+    :type pts: list[(float, float)] | numpy.ndarray
+    :type num_vert: int
+    :rtype: list[int]
+    """
     return dp_poly_close[double](pts, num_vert)
